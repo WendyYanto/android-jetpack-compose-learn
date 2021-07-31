327b98bf44cf2be3f112264287a4d9a22c1804d9
wendyyanto <yanto.wendy98@gmail.com>
2021-07-31 23:11:53 +0700

    feature: setup modifier for padding

--- app/src/main/java/dev/wendyyanto/jetpackcomposelearn/MainActivity.kt
+++ app/src/main/java/dev/wendyyanto/jetpackcomposelearn/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.widget.Toolbar
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.wendyyanto.jetpackcomposelearn.ui.theme.JetpackComposeLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +32,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = Color.Yellow) {
-        Text(text = "Hello $name!")
+        Text(text = "Hello $name!",
+            modifier = Modifier.padding(16.dp))
     }
 }
 
